6046876dfedeacfbad3573ca8eea9822f9fae4b6
Student05 <sombat@snru.ac.th>
2017-06-14 16:20:56 +0700

    step 2

--- NavigatorToolbarMultipleScreen/ViewController.swift
+++ NavigatorToolbarMultipleScreen/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pageNum = 1;
+    
+    @IBAction func increaseNumber(_ sender: Any) {
+        pageNum += 1
+        if pageNum > 10 {
+           pageNum = -10
+        }
+        numberLabel.text = "\(pageNum)"
+        
+        
+    }
 
+    @IBAction func decreaseNumber(_ sender: Any) {
+        pageNum -= 1
+        if pageNum < -10 {
+           pageNum = 10
+        }
+        numberLabel.text = "\(pageNum)"
+    }
+    
+    
+    
+    @IBAction func resetNumber(_ sender: Any) {
+        
+        pageNum = 1
+        
+        numberLabel.text = "\(pageNum)"
+    }
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
+    func showMessage(strMessage : String) -> Void {
+        print("Message==>\(strMessage)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
